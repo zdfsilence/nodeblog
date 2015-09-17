2dc62914486d41d13a6b4cbad1bcef924111beab
zdf6300 <zdf6300@LY.com>
2015-09-17 16:27:07 +0800

    1

--- config.js
+++ config.js
@@ -6,6 +6,9 @@ module.exports = {
 
 	name: 'Blog', // 社区名字
 	description: '一群人的博客', // 社区的描述
-	keywords: 'nodejs, node, express, connect, socket.io'
+	keywords: 'nodejs, node, express, connect, socket.io',
 
+	// mongodb 配置
+  	db: 'mongodb://10.1.205.42:27017/',
+  	
 }
